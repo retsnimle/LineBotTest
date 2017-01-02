fc3ffba9ca0ee7d59e2d2009ce001867344249f7
retsnimle <retsnimle@gmail.com>
2017-01-01 23:03:13 -0600

    Update index.js

--- index.js
+++ index.js
@@ -272,7 +272,7 @@ function CoC7th(inputStr){
     ReStr = ReStr + '\nＳＩＺ：' + DiceCal('(2d6+6)*5');
     if (old<20) ReStr = ReStr + ' ←擇一減' + Debuff ;
     ReStr = ReStr + '\nＩＮＴ：' + DiceCal('(2d6+6)*5');         
-    if (old<20) ReStr = ReStr + '\nＥＤＵ：' + DiceCal('3d6*5-5');
+    if (old<20) ReStr = ReStr + '\nＥＤＵ：' + DiceCal('(2d6+6)*5-5');
     else {
       let firstEDU = '(' + RollDice('2d6') + '+6)*5';
       ReStr = ReStr + '\n==';

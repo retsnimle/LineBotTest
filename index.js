5fe0c6dec3fcbc58ffa24f924d35244452b49b36
retsnimle <retsnimle@gmail.com>
2017-08-22 16:56:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -99,8 +99,6 @@ function parseInput(rplyToken, inputStr) {
         if (inputStr.toLowerCase().match(/^cc/)!= null) return CoC7th(inputStr.toLowerCase()) ;      
         else
         //入幫測驗判定在此
-          if (inputStr.match('ybb') != null) return Yababang(inputStr) ;      
-          else
         if (inputStr.match('鴨霸幫入幫測驗') != null) return Yababang(inputStr) ;      
         else
         //pbta判定在此
